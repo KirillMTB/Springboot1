aa1d08e363befa92c10dcf70a76ab02daae35479
KirillMTB <154924670+KirillMTB@users.noreply.github.com>
2024-12-14 19:03:32 +0400

    spring boot

---
+++ src/main/java/ru/example/version2/Version2Application.java
@@ -0,0 +1,13 @@
+package ru.example.version2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Version2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Version2Application.class, args);
+	}
+
+}
