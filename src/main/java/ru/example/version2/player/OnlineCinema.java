928a763f68f8a02133a7d9fe824e2b656e3b04db
KirillMTB <154924670+KirillMTB@users.noreply.github.com>
2025-02-28 12:16:01 +0400

    28/02/2024

--- src/main/java/ru/example/version2/player/OnlineCinema.java
+++ src/main/java/ru/example/version2/player/OnlineCinema.java
@@ -1,6 +1,6 @@
 package ru.example.version2.player;
 
-public class Demo {
+public class OnlineCinema {
     public static void main(String[] args) {
         Player player = new Player();
         UI ui = new UI(player);
